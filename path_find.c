6e1dbc442dc08317410ae66adc8958e9cb6ee411
mrfouu <bryanweinegaessel@gmail.com>
2025-01-09 14:49:04 +0100

    correctif

--- path_find.c
+++ path_find.c
@@ -7,23 +7,38 @@
  */
 char *find_path(char *command)
 {
-	char *path = getenv("PATH");
-	char *path_copy = strdup(path);
-	char *dir = strtok(path_copy, ":");
-	char *full_path = malloc(1024);
+	char *path, *path_copy, *dir, *full_path;
+	size_t len;
+	struct stat st;
 
+	if (stat(command, &st) == 0 && (st.st_mode & S_IXUSR))
+		return (strdup(command));
+	path = getenv("PATH");
+	if (!path)
+		return (NULL);
+	path_copy = strdup(path);
+	if (!path_copy)
+		return (NULL);
+	dir = strtok(path_copy, ":");
 	while (dir != NULL)
 	{
-		snprintf(full_path, 1024, "%s/%s", dir, command);
-		if (access(full_path, X_OK) == 0)
+		len = strlen(dir) + strlen(command) + 2;
+		full_path = malloc(len);
+		if (!full_path)
+		{
+			free(path_copy);
+			return (NULL);
+		}
+		printf(full_path, len, "%s/%s", dir, command);
+		if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
 		{
 			free(path_copy);
 			return (full_path);
 		}
+		free(full_path);
 		dir = strtok(NULL, ":");
 	}
-
 	free(path_copy);
-	free(full_path);
 	return (NULL);
 }
+
